c3f7f2b1e5e86c9980ff0d2f5348144f23fc0466
Cybernetics <mail@cybernetics.me>
2023-12-12 08:40:58 +0530

    small updates

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -43,7 +43,7 @@
                                             <td>{{ $key + 1 }}</td>
                                             <td>{{ $item->name }}</td>
                                             <td>{{ $item->type?->name }}</td>
-                                            <td><a href="{{ $item->image }}" target="_blank"><i class="fa fa-image text-info fa-lg"></i></a></td>
+                                            <td><a href="{{ asset($item->image) }}" target="_blank"><i class="fa fa-image text-info fa-lg"></i></a></td>
                                             <td class="text-center">{{ $item->display_order }}</td>
                                             <td>{!! $item->categoryStatus() !!}</td>
                                             <td>{!! $item->deletedStatus() !!}</td>

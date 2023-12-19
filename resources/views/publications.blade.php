23247945da555e1a42ff003da4e74c29bc42bad2
Cybernetics <mail@cybernetics.me>
2023-12-19 18:07:40 +0530

    small updates

--- resources/views/publications.blade.php
+++ resources/views/publications.blade.php
@@ -1,26 +1,37 @@
 @extends("base")
 @section("content")
+<div class="page_header_default style_one ">
+    <div class="parallax_cover">
+        <img src="{{ asset('/frontend/assets/images/page-header-default.jpg') }}" alt="bg_image" class="cover-parallax">
+    </div>
+    <div class="page_header_content">
+        <div class="auto-container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="banner_title_inner">
+                        <div class="title_page">
+                            Publications
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-12">
+                    <div class="breadcrumbs creote">
+                        <ul class="breadcrumb m-auto">
+                            <li><a href="/">Home</a> </li>
+                            <li class="active">Publications</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <!--===============PAGE CONTENT==============-->
 <div id="content" class="site-content ">
     <div class="container-fluid">
         <div class="row default_row">
             <div class="full_width_box">
-                <div class="pd_top_80"></div>
-                <div class="row">
-                    <div class="col-md-6 col-sm-6 col-lg-6">
-                        <div class="heading mb-3">
-                            <h2>Publications</h2>
-                        </div>
-                    </div>
-                    <div class="col-md-6 col-sm-6 col-lg-6 text-end">
-                        <div class="breadcrumbs creote">
-                            <ul class="breadcrumb m-auto">
-                                <li><a href="/">Home</a> </li>
-                                <li class="active">Publications</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+                <div class="pd_top_40"></div>
                 <div class="row mt-5">
                     @forelse($categories as $key => $item)
                     <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-xs-12">
@@ -41,7 +52,7 @@
                     @endforelse
                 </div>
                 <!--===============spacing==============-->
-                <div class="pd_bottom_70"></div>
+                <div class="pd_bottom_40"></div>
                 <!--===============spacing==============-->
             </div>
         </div>

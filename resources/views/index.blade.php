a7fa58e85e1509e678fd7584a1f7281da96b507b
Cybernetics <mail@cybernetics.me>
2023-12-23 10:07:39 +0530

    small updates

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -86,10 +86,6 @@
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
-    <section class="image_grid_content style_one image_covered">
-        <div class="container">
             <div class="row">
                 <div class="col-xl-4 col-lg-6 col-md-6 col-sm-12 col-xs-12">
                     <div class="mb-3">

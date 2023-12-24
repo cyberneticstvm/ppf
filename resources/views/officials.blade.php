9bb3705018bc0dbe7adc756ae1c357efaab6e78e
Cybernetics <mail@cybernetics.me>
2023-12-24 08:57:47 +0530

    small updates

--- resources/views/officials.blade.php
+++ resources/views/officials.blade.php
@@ -35,9 +35,9 @@
         <div class="container">
             <div class="row">
                 @forelse($officials->where('panel', 'office_bearer') as $key => $item)
-                <div class="col-sm-3">
-                    <div class="team_box style_three">
-                        <div class="team_box_outer">
+                <div class="col-lg-3">
+                    <div class="team_box style_two">
+                        <div class="team_box_outer" style="padding: 15%;">
                             <div class="member_image">
                                 <img src="{{ asset($item->image) }}" alt="Official image" />
                             </div>
@@ -90,8 +90,8 @@
             <div class="row">
                 @forelse($officials->where('panel', 'executive_member') as $key => $item)
                 <div class="col-lg-3 col-md-4 col-sm-12 col-xs-12">
-                    <div class="team_box style_one">
-                        <div class="team_box_outer">
+                    <div class="team_box style_two">
+                        <div class="team_box_outer" style="padding: 15%;">
                             <div class="member_image">
                                 <img src="{{ asset($item->image) }}" alt="Official image" />
                             </div>
@@ -144,8 +144,8 @@
             <div class="row">
                 @forelse($officials->where('panel', 'auditor') as $key => $item)
                 <div class="col-lg-3 col-md-4 col-sm-12 col-xs-12">
-                    <div class="team_box style_one">
-                        <div class="team_box_outer">
+                    <div class="team_box style_two">
+                        <div class="team_box_outer" style="padding: 15%;">
                             <div class="member_image">
                                 <img src="{{ asset($item->image) }}" alt="Official image" />
                             </div>

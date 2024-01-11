3fbcf316389d7703cf277b7606465d9a9c00e4d6
Cybernetics <mail@cybernetics.me>
2024-01-11 11:24:15 +0530

    small updates

--- public/frontend/assets/js/main.js
+++ public/frontend/assets/js/main.js
@@ -1,11 +1,11 @@
 $(function () {
     "use strict"
-    $(window).scroll(function () {    // this will work when your window scrolled.
-        var height = $(window).scrollTop();  //getting the scrolling height of window
+    /*$(window).scroll(function () {    
+        var height = $(window).scrollTop();  
         if (height > 100) {
             $(".scroolMsg").css('display', 'none')
         } else {
             $(".scroolMsg").css('display', 'block')
         }
-    });
+    });*/
 })

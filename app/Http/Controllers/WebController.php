4063bab8b8081754ba330bfadc5fca43aa28ba60
Cybernetics <mail@cybernetics.me>
2024-02-12 09:39:02 +0530

    small updates

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -220,7 +220,7 @@ class WebController extends Controller
 
     public function sendnotificationemail()
     {
-        $members = Membership::whereNotNull('email')->whereNot('email', '')->offset(23)->limit(25)->get();
+        $members = Membership::whereNotNull('email')->whereNot('email', '')->offset(48)->limit(25)->get();
         foreach ($members as $key => $member) :
             Mail::to($member->email)->send(new NotificationEmail($member));
         endforeach;

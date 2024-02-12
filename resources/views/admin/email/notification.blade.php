99821e48e88ae330db238e4a395ccd5b55121750
Cybernetics <mail@cybernetics.me>
2024-02-12 09:37:29 +0530

    small updates

--- resources/views/admin/email/notification.blade.php
+++ resources/views/admin/email/notification.blade.php
@@ -9,7 +9,7 @@
     Dear {{ $member->name }},<br /><br />
     Members can login to our PPF website <a href="https://ppfkuwait.org/login" target="_blank">www.ppfkuwait.org</a><br />
 
-    login ID is your membership number. If you forgot your password, you can use change password option on the login screen to request the password reset link. Once your request has been completed, the password reset link will be sent to your registered email id.<br />
+    login ID is your membership number ({{ $member->membership_number }}). If you forgot your password, you can use change password option on the login screen to request the password reset link. Once your request has been completed, the password reset link will be sent to your registered email id.<br />
 
     If you know your password and want to change the same, sign in using existing credentials and use Change Password option which you can see the left upper corner and below the profile image.<br />
 

16c1f3d0798d0e8ac4bc81e971d3f6ff3d0a3f46
Cybernetics <mail@cybernetics.me>
2024-05-31 13:36:26 +0530

    small updates

--- resources/views/admin/member/edit.blade.php
+++ resources/views/admin/member/edit.blade.php
@@ -317,7 +317,7 @@
                                         <label class="col-form-label pt-0" for="next_renewal_date">Next Renewal Date </label>
                                         {{ html()->date('next_renewal_date', $member->next_renewal_date?->format('Y-m-d') ?? date('Y-m-d', strtotime('+1 year')))->class('form-control') }}
                                     </div>
-                                    <div class="col-md-2">
+                                    <div class="col-md-3">
                                         <label class="col-form-label pt-0 req" for="referred_by">Referred by </label>
                                         {{ html()->select('referred_by', $members->pluck('name', 'id'), $member->referred_by)->class('form-control')->placeholder('Select') }}
                                     </div>

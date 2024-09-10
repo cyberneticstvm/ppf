67449e4ed19df1dc4eef9058c2f672941020b4a1
Cybernetics <mail@cybernetics.me>
2024-09-10 09:12:26 +0530

    small updates

--- app/Exports/MembersExport.php
+++ app/Exports/MembersExport.php
@@ -24,30 +24,31 @@ class MembersExport implements FromCollection, WithHeadings, ShouldAutoSize, Wit
 
     public function collection()
     {
-        $members = Membership::selectRaw("membership_number, name, type, email, kw_primary_contact_number, kw_area, specialization, qualification, in_district, referred_by, doj, approved_date, next_renewal_date")->where('approval_status', $this->status)->get();
+        $members = Membership::selectRaw("membership_number, civil_id, name, type, email, kw_primary_contact_number, kw_area, specialization, qualification, in_district, referred_by, doj, approved_date, next_renewal_date")->where('approval_status', $this->status)->get();
         return $members->map(function ($data, $key) {
             return [
                 '1' =>  $key + 1,
                 '2' => $data->membership_number,
-                '3' => $data->name,
-                '4' => $data->type,
-                '5' => $data->email,
-                '6' => $data->kw_primary_contact_number,
-                '7' => $data->kw_area,
-                '8' => $data->specializations?->name,
-                '9' => $data->qualifications?->name,
-                '10' => $data->in_district,
-                '11' => $data->members?->name,
-                '12' => $data->doj?->format('d.M.Y'),
-                '13' => $data->approved_date?->format('d.M.Y'),
-                '14' => $data->next_renewal_date?->format('d.M.Y'),
+                '3' => $data->civil_id,
+                '4' => $data->name,
+                '5' => $data->type,
+                '6' => $data->email,
+                '7' => $data->kw_primary_contact_number,
+                '8' => $data->kw_area,
+                '9' => $data->specializations?->name,
+                '10' => $data->qualifications?->name,
+                '11' => $data->in_district,
+                '12' => $data->members?->name,
+                '13' => $data->doj?->format('d.M.Y'),
+                '14' => $data->approved_date?->format('d.M.Y'),
+                '15' => $data->next_renewal_date?->format('d.M.Y'),
             ];
         });
     }
 
     public function headings(): array
     {
-        return ['SL No', 'Membership ID', 'Name', 'Type', 'Email', 'Primary Contact', 'Place / Area', 'Industry / Specialization', 'Qualification', 'District', 'Referred by', 'Join Date', 'Approved Date', 'Next Renewal Date'];
+        return ['SL No', 'Membership ID', 'Civil ID', 'Name', 'Type', 'Email', 'Primary Contact', 'Place / Area', 'Industry / Specialization', 'Qualification', 'District', 'Referred by', 'Join Date', 'Approved Date', 'Next Renewal Date'];
     }
 
     /*public function map($data): array

4e6bd1265e99907deb9a46fe9ccd0624cd00aa39
Cybernetics <mail@cybernetics.me>
2026-04-02 09:11:13 +0530

    small updates

--- resources/views/admin/member/edit.blade.php
+++ resources/views/admin/member/edit.blade.php
@@ -330,7 +330,7 @@
                                         {{ html()->select('area', $areas, $member->area)->class('form-control')->placeholder('Select') }}
                                     </div>
                                     <div class="col-md-3">
-                                        <label class="col-form-label pt-0" for="area">Send Renewal Notification </label>
+                                        <label class="col-form-label pt-0" for="area">Send Renewal Notification </label><br>
                                         {{ html()->checkbox('send_renewal_notification_email', false, '1')->class('form-checkbox') }}
                                     </div>
                                     @endif
